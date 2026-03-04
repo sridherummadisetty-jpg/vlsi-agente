ff7a1d5c77426bc83dd94dc99a2d6c11df50a9ab
sridherummadisetty-jpg <sridherummadisetty@gmail.com>
2026-03-04 20:52:42 +0530

    Update src/main.tsx to use React 19 createRoot API instead of deprecated ReactDOM.render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
     return (
@@ -10,4 +10,7 @@ const App = () => {
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    createRoot(rootElement).render(<App />);
+}
